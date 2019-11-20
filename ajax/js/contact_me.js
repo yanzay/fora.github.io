f092098ff5351908232267de6e03f4c185b7eba4
Alexey Grachov <grachov.alexey@gmail.com>
2019-11-20 17:47:07 +0200

    Enable button.

--- ajax/js/contact_me.js
+++ ajax/js/contact_me.js
@@ -19,6 +19,9 @@ $(function() {
       }
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+      setTimeout(function() {
+        $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+      }, 1000);
       const req = {
         name: name,
         phone: phone,
